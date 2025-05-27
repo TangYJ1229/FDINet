b94f8d3239f676dc9df26583c8c47def8e30e6b8
Tang <79182380+TangYJ1229@users.noreply.github.com>
2025-05-27 14:29:02 +0800

    Delete FDINet directory

--- FDINet/run.sh
+++
@@ -1,5 +0,0 @@
-python training.py --datapath "/home/tang/TANG/Dataset/ChangeDetection/WH/data_256"
-
-python training.py --datapath "/home/tang/TANG/Dataset/ChangeDetection/SECOND/data"
-
-python training.py --datapath "/home/tang/TANG/Dataset/ChangeDetection/CDD_fine/data"
